c097d535d112458e7240b3522a1f80388589acc7
Yash Gupta <yashprakash28@users.noreply.github.com>
2021-06-20 02:03:11 +0530

    Solar Leap script updated

--- POC/Assets/SolarScript.cs
+++ POC/Assets/SolarScript.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class SolarScript : MonoBehaviour
 {
     public GameObject ledPanel;
+    private GameObject[] lights;
+    private int noLights = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lights = new GameObject[ledPanel.transform.childCount];
+        for(int i=0 ; i<ledPanel.transform.childCount ; i++)
+        {
+            lights[i] = ledPanel.transform.GetChild(i).gameObject;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // distance = Vector3.Distance(solarPanel.transform.position, leapHand.transform.position);
-        // Debug.Log(distance);
-        // Debug.Log(leapHand.transform.position);
+
     }
 
     public void printActivate()
     {
         Debug.Log("activated");
+        foreach (GameObject l in lights)
+        {
+            l.GetComponent<Light>().intensity = 0.3f;
+        }
     }
 
     public void printDeActivate()
     {
         Debug.Log("De activated");
+        foreach (GameObject l in lights)
+        {
+            l.GetComponent<Light>().intensity = 0f;
+        }
     }
 }
